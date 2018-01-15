cb94c982f356a55a4c1a8a272915865b9680eae1
Rabid Scallion <Doug@dougs-macmini.local>
2018-01-15 17:07:43 -0500

    init commit

---
+++ windows-shopper/windows-shopper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  windows-shopper
+//
+//  Created by Doug Heaton on 1/15/18.
+//  Copyright © 2018 Trusted Tiger LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
